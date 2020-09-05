50b826f464873ef917f063b1d6cf744a087fdc42
Umaidasim <umaidasim212@gmail.com>
2020-09-05 20:15:01 +0500

    Updates

---
+++ precache-manifest.62d697feada6abf0d06e383b16c5f35b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "ada3dcfd6e754e1f3345",
+    "url": "/umaid/static/css/main.6f1a1477.chunk.css"
+  },
+  {
+    "revision": "ada3dcfd6e754e1f3345",
+    "url": "/umaid/static/js/main.c21517b3.chunk.js"
+  },
+  {
+    "revision": "c4e54650c0e9bfc7f7be",
+    "url": "/umaid/static/js/runtime~main.e01b1e8f.js"
+  },
+  {
+    "revision": "3a7462081d2a1ca46f5f",
+    "url": "/umaid/static/js/2.4c7d6b81.chunk.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/umaid/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/umaid/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/umaid/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "efce2fe7ee524df7e39d48614646a549",
+    "url": "/umaid/static/media/autonomous-car.efce2fe7.png"
+  },
+  {
+    "revision": "e2a441f422e86b08d9a3bf15207323d7",
+    "url": "/umaid/static/media/drowsiness_det.e2a441f4.png"
+  },
+  {
+    "revision": "43791c9f36dab254adc837e0bee810fa",
+    "url": "/umaid/static/media/Accident-Detection.43791c9f.png"
+  },
+  {
+    "revision": "40a463cb42f6a285db1785d9f0f6aa6c",
+    "url": "/umaid/static/media/matlabmodel.40a463cb.png"
+  },
+  {
+    "revision": "66a2b4326dd8ea06eeaf896c8fab5e52",
+    "url": "/umaid/static/media/PowerSystem.66a2b432.png"
+  },
+  {
+    "revision": "b5116e9c3dc64421ec98e8f3302e6d15",
+    "url": "/umaid/static/media/Sensviz4.b5116e9c.png"
+  },
+  {
+    "revision": "480889ea645c3cd30df35ddbe0269a5d",
+    "url": "/umaid/static/media/skill-development.480889ea.png"
+  },
+  {
+    "revision": "675d9d3bb7bc12ddd81b29fc288e4c79",
+    "url": "/umaid/static/media/services.675d9d3b.png"
+  },
+  {
+    "revision": "6da9c3f583e8398505eddff333690b26",
+    "url": "/umaid/static/media/Contact-Us.6da9c3f5.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/umaid/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "82fa80f9791f4bfba64e49f99cb89eed",
+    "url": "/umaid/index.html"
+  }
+];
